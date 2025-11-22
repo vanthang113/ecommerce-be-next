f49b3cf61ede5e93ca6f6f5798c89cc168da6f06
vanthang113 <vanthang2222147@gmail.com>
2025-11-22 17:57:52 +0700

    sửa database db.ts và env

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,23 @@
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const pool = mysql.createPool({
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER || "root",
+//   password: process.env.DB_PASS || "12345",
+//   database: process.env.DB_NAME || "ecommerce_db",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// });
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "12345",
-  database: process.env.DB_NAME || "ecommerce_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const dbUrl = process.env.DB_URL;
+
+if (!dbUrl) {
+  throw new Error("DB_URL is not defined in .env");
+}
+
+export const pool = mysql.createPool(dbUrl);
