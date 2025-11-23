11221ae7ff24533aa5b3e8ce8b12b5d741d2a142
vanthang113 <vanthang2222147@gmail.com>
2025-11-23 17:19:09 +0700

    sửa app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,7 @@ import categoryRoutes from "./routes/category.routes";
 dotenv.config();
 const app = express();
 
-// app.use(cors());
-app.use(cors({
-  origin: 'https://ecommerce-fe-next-thangs-projects-f870cc3a.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 
 app.use("/api/auth", authRoutes);
