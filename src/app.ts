636024bfac8e448fcc1941499d4cfb95014f539b
vanthang113 <vanthang2222147@gmail.com>
2025-11-23 16:45:57 +0700

    sửa app.ts cho phép truy cập fe

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,7 @@ import categoryRoutes from "./routes/category.routes";
 dotenv.config();
 const app = express();
 
-// app.use(cors());
-app.use(cors({
-  origin: 'https://ecommerce-fe-next-eight.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 
 app.use("/api/auth", authRoutes);
